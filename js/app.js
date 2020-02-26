5c07fb78346f90d3a2bf784331e62a1736c8905f
Catalina Carreto <isccatalinacarreto27@gmail.com>
2020-02-26 17:16:11 -0600

    feat: Validar campos que no esten vacios

--- js/app.js
+++ js/app.js
@@ -11,10 +11,58 @@ function eventListeners(){
     
     //Inicio de la aplicacion y deshabilitar submit
     document.addEventListener('DOMContentLoaded', inicioApp);
+
+    //Campos del formulario
+    email.addEventListener('blur',validarCampo);
+    asunto.addEventListener('blur', validarCampo);
+    mensaje.addEventListener('blur', validarCampo);
 }
 
 //Funciones
 function inicioApp(){
     //deshabilitar el envio
     btnEnviar.disabled = true;
+}
+
+//Valida que el campo tenga algo escrito
+function validarCampo(){
+
+    //Se valida la longitud del texto y que no este vacio
+    validarLongitud(this);
+
+    //valida unicamente el email
+    if(this.type === 'email'){
+        validarEmail(this);
+    }
+
+    let errores = document.querySelectorAll('.error');
+
+    if(email.value !== '' && asunto.value !== '' && mensaje.value !== ''){
+        if(errores.length === 0){
+            btnEnviar.disabled = false;
+        }  
+    }
+}
+
+function validarLongitud(campo){
+
+    if(campo.value.length > 0 ){
+        campo.style.borderBottomColor = 'green';
+        campo.classList.remove('error');
+    }else{
+        campo.style.borderBottomColor = 'red';
+        campo.classList.add('error');
+    }
+}
+
+//Validar email
+function validarEmail(campo){
+    const mensaje = campo.value;
+    if(mensaje.indexOf('@') !== -1) {
+        campo.style.borderBottomColor = 'green';
+        campo.classList.remove('error');
+    }else{
+        campo.style.borderBottomColor = 'red';
+        campo.classList.add('error');
+    }
 }
